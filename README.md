4d73035a9a9bfd0753ba14b74cc8b3990cfc8c5d
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-10-03 23:37:06 +0200

    initial commit of @LastPass @Ansible inventory script
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,74 @@
+# Dynamic LastPass Ansible Inventory
+
+Requires a [LastPass](https://www.lastpass.com/) account and the [lpass](https://github.com/lastpass/lastpass-cli)
+client.
+
+The `lastpass_inventory.py` script uses the `lastpass_inventory.yml`
+configuration file to fetch the named servers from the current logged in
+LastPass account using `lpass`, and then generate an [Ansible](https://www.ansible.com/)
+inventory.
+
+## Examples and Usage
+
+The `lastpass_inventory.yml` configuration file names and fetches the hosts
+using `name: identifier`, where the `name` variable is what the host will be
+named in the inventory and the `identifier` is used to identify the host in
+LastPass.
+
+Both the name and the ID number from e.g. `lpass ls` can be used as an
+identifier for a host.
+
+Note that the script will fail if the name or ID is incorrect or missing.
+
+The LastPass `user` and `password` fields will be used to populate
+`ansible_user` and `ansible_password`.
+The `ansible_host` address is the extracted netloc from the LastPass `url` field
+using [urllib.parse.urlparse](https://docs.python.org/3/library/urllib.parse.html).
+
+Example `lpass ls` output:
+
+```console
+servers
+    ansible-managed
+        test-server-01 [id: 3024014487647346952]
+        test-server-02 [id: 7815456364361241116]
+```
+
+Configuration file based on the about output:
+
+```yaml
+---
+lastpass_hosts:
+    test-server-01: test-server-01
+    test-server-02: test-server-02
+    webserver: test-server-01
+    database: "7815456364361241116"
+...
+```
+
+Running `ansible-inventory -i lastpass_inventory.py --list --yaml` will then
+generate the following inventory:
+
+```yaml
+all:
+  children:
+    lastpass_hosts:
+      hosts:
+        database:
+          ansible_host: 10.0.1.2
+          ansible_password: SuperSecretLastPass
+          ansible_user: ansibleuser
+        test-server-01:
+          ansible_host: 10.0.1.1
+          ansible_password: SuperSecretLastPass
+          ansible_user: ansibleuser
+        test-server-02:
+          ansible_host: 10.0.1.2
+          ansible_password: SuperSecretLastPass
+          ansible_user: ansibleuser
+        webserver:
+          ansible_host: 10.0.1.1
+          ansible_password: SuperSecretLastPass
+          ansible_user: ansibleuser
+    ungrouped: {}
+```
---
+++ lastpass_inventory.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+"""Populate a Ansible inventory with information from LastPass."""
+import os
+import shutil
+import subprocess
+import sys
+import argparse
+from urllib.parse import urlparse
+import yaml
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+__version__ = "0.0.1"
+
+
+class AnsibleLastPassInventory:
+    """The AnsibleLastPassInventory class."""
+
+    def __init__(self):
+        self.lastpass_cmd = shutil.which("lpass")
+        self.inventory = {}
+        self.inventory_content = {}
+        self.generated_inventory = {}
+        self.read_cli_args()
+        self.ensure_lastpass()
+        self.generate_inventory()
+
+    def generate_inventory(self):
+        """Generate the inventory."""
+        try:
+            self.configuration_file = os.getcwd() + "/lastpass_inventory.yml"
+
+            if not os.path.isfile(self.configuration_file):
+                print(self.configuration_file + " can't be found.")
+                sys.exit(1)
+            else:
+                with open(
+                    "./lastpass_inventory.yml", "r", encoding="utf-8"
+                ) as inventory_file:
+                    self.inventory = yaml.safe_load(inventory_file)
+
+            self.generated_inventory = self.inventory_content
+            self.inventory_content["lastpass_hosts"] = []
+            self.inventory_content["_meta"] = {}
+            self.inventory_content["_meta"]["hostvars"] = {}
+
+            for inventory in self.inventory.values():
+                for name, identifier in inventory.items():
+                    host_lpass_json = json.loads(
+                        subprocess.run(
+                            [self.lastpass_cmd, "show", identifier, "--json"],
+                            shell=False,
+                            check=True,
+                            text=True,
+                            capture_output=True,
+                        ).stdout
+                    )
+
+                    self.inventory_content["lastpass_hosts"].append(name)
+                    self.inventory_content["_meta"]["hostvars"][name] = {}
+                    self.inventory_content["_meta"]["hostvars"][name][
+                        "ansible_user"
+                    ] = host_lpass_json[0]["username"]
+                    self.inventory_content["_meta"]["hostvars"][name][
+                        "ansible_password"
+                    ] = host_lpass_json[0]["password"]
+                    self.inventory_content["_meta"]["hostvars"][name][
+                        "ansible_host"
+                    ] = urlparse(host_lpass_json[0]["url"]).netloc
+
+        except subprocess.CalledProcessError as exception:
+            print("There was an issue with:\n  " + name + ": " + identifier)
+            print(exception)
+            sys.exit(1)
+
+        if self.args.list:
+            print(json.dumps(self.generated_inventory, sort_keys=True, indent=2))
+        else:
+            print(json.dumps(self.generated_inventory, sort_keys=True))
+
+    def ensure_lastpass(self):
+        """Ensure LastPass is installed and the user is logged in."""
+        try:
+            if not self.lastpass_cmd:
+                print("lpass doesn't seem to be installed. Exiting.")
+                sys.exit(1)
+
+            if not subprocess.check_output([self.lastpass_cmd, "ls"], shell=False):
+                sys.exit(1)
+
+        except Exception as exception_string:
+            print("Exception: ", str(exception_string), file=sys.stderr)
+            sys.exit(1)
+
+    def list_lastpass_vault(self):
+        """Test the lpass ls function."""
+        try:
+            subprocess.run([self.lastpass_cmd, "ls"], shell=False, check=True)
+
+        except Exception as exception_string:
+            print("Exception: ", str(exception_string), file=sys.stderr)
+            sys.exit(1)
+
+    def read_cli_args(self):
+        """Command line arguments and help information."""
+
+        parser = argparse.ArgumentParser(
+            description="Populate a Ansible inventory with information from LastPass.",
+            epilog="version: " + __version__,
+        )
+        parser.add_argument(
+            "-l",
+            "--list",
+            help="print the inventory",
+            action="store_true",
+        )
+
+        self.args = parser.parse_args()
+
+
+AnsibleLastPassInventory()
